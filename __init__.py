8116c3024759a3296d46373b47f94cd0f4e2038f
newban <zshifu@mail.ustc.edu.cn>
2016-01-26 15:34:06 +0800

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from DELCgen import Mixture_Dist, Min_PDF, OptBins, BendingPL
+from DELCgen import Simulate_DE_Lightcurve, Simulate_TK_Lightcurve
+from DELCgen import TimmerKoenig, EmmanLC
+
+__all__ = ['Mixture_Dist', 'BendingPL', 'RandAnyDist', 'Min_PDF','OptBins',
+            'PSD_Prob','SD_estimate','TimmerKoenig','EmmanLC','Lightcurve',
+            'Comparison_Plots','Load_Lightcurve','Simulate_TK_Lightcurve',
+            'Simulate_DE_Lightcurve']
